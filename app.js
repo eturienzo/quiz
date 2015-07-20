6f26e3f5c12e8b66d983bf37f98b26ef6bc2a46c
eturienzo <eletury@gmail.com>
2015-07-20 22:31:15 +0200

    Auto-logout de sesión

--- app.js
+++ app.js
@@ -40,6 +40,19 @@ app.use(function(req, res, next) {
   next();
 });
 
+//Autologout
+app.use(function(req, res, next) {
+	if (req.session.user) {
+		if (Date.now() - req.session.user.lastRequestTime > 120000) {
+			delete req.session.user;
+		} else {
+			req.session.user.lastRequestTime = Date.now();
+	}
+}
+next();
+});
+
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
